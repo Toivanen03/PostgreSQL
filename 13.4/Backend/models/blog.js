2216ca775a55d0b24e3e34ee2d9457cbd10bd165
Simo Toivanen <posti@simotoivanen.fi>
2025-05-04 05:34:31 +0300

    13.4 palautus

--- 13.4/Backend/models/blog.js
+++ 13.4/Backend/models/blog.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize')
+const { Model, DataTypes, Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.EXTERNAL_DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+        },
     },
-  },
 })
 
 class Blog extends Model {}
@@ -37,14 +37,6 @@ Blog.init({
   modelName: 'Blog'
 })
 
-sequelize.authenticate()
-  .then(async () => {
-    const [results] = await sequelize.query('SELECT * FROM blogs')
-    results.forEach(blog => {
-      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
-    })
-    await sequelize.close()
-  })
-  .catch(err => {
-    console.error('Virhe: ', err)
-  })
+Blog.sync()
+
+module.exports = { Blog }
--- 13.4/Backend/src/cli.js
+++ 13.4/Backend/src/cli.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize')
+const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.EXTERNAL_DATABASE_URL, {
   dialectOptions: {
@@ -10,36 +10,9 @@ const sequelize = new Sequelize(process.env.EXTERNAL_DATABASE_URL, {
   },
 })
 
-class Blog extends Model {}
-Blog.init({
-  author: DataTypes.STRING,
-  url: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  },
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'Blog'
-})
-
 sequelize.authenticate()
   .then(async () => {
-    const [results, metadata] = await sequelize.query('SELECT * FROM blogs')
+    const [results] = await sequelize.query('SELECT * FROM blogs')
     results.forEach(blog => {
       console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
     })
